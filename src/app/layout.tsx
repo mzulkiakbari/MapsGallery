ef103fbdaba3751bad62a94bacb57d02c8436edf
mzulkiakbari <blomenid@gmail.com>
2026-03-23 03:18:04 +0700

    Add SpeedInsight

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { GalleryProvider } from "@/context/GalleryContext";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={inter.className} suppressHydrationWarning>
         <GalleryProvider>
           {children}
+          <SpeedInsights />
         </GalleryProvider>
       </body>
     </html>
